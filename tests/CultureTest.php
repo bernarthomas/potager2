27d60f68e1c417c1bb7dc7e79d02fb0891a88cf2
Bernard Thomas <bernarthomas@free.fr>
2022-02-02 01:33:48 +0100

    testAffichagePageListeCultures vert

--- tests/CultureTest.php
+++ tests/CultureTest.php
@@ -3,14 +3,19 @@ namespace App\Tests;
 
 use \Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Router;
 
 class CultureTest extends WebTestCase
 {
     public function testAffichagePageListeCultures()
     {
         $client = static::createClient();
-        $response = $client->request('GET', '/');
-        $this->assertResponseIsSuccessful();
-        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $client->request('GET', 'http://potager2/cultures');
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
+    protected static function getKernelClass(): string
+    {
+        return '\App\Kernel';
     }
 }
