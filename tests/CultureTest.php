846a3a88c6339eb9d6067f7450f40322594ddaea
Bernard Thomas <bernarthomas@free.fr>
2022-02-02 01:39:35 +0100

    testAffichagePageListeCultures vert

--- tests/CultureTest.php
+++ tests/CultureTest.php
@@ -3,7 +3,6 @@ namespace App\Tests;
 
 use \Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Router;
 
 class CultureTest extends WebTestCase
 {
